171621563ee2d7a1a99f20b439831da3aad1d6b6
Valerii Odintsov <skif1998@gmail.com>
2025-07-16 19:51:51 +0200

    Add feature: new handlers functions, commands in main

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ Handles command parsing and control loop.
 from src.models.storage import load_data, save_data
 from src.handlers.utils import parse_input
 from src.handlers.handlers import (
-    add_contact, change_contact, show_phone,
-    show_all, set_birthday, show_birthday, birthdays,
+    add_contact, change_contact, show_phone, show_all,
+    set_address, show_address, remove_address, add_email,
+    change_email, show_email, remove_email, find_email,
+    set_birthday, show_birthday, remove_birthday, birthdays,
     remove_phone, find_phone, delete_contact, help_command
 )
 
@@ -37,10 +39,28 @@ def main():
                 print(show_phone(args, book))
             case "all":
                 print(show_all(book))
+            case "add-address":
+                print(set_address(args, book))
+            case "show-address":
+                print(show_address(args, book))
+            case "remove-address":
+                print(remove_address(args, book))
+            case "add-email":
+                print(add_email(args, book))
+            case "change-email":
+                print(change_email(args, book))
+            case "show-email":
+                print(show_email(args, book))
+            case "remove-email":
+                print(remove_email(args, book))
+            case "find-email":
+                print(find_email(args, book))
             case "set-birthday":
                 print(set_birthday(args, book))
             case "show-birthday":
                 print(show_birthday(args, book))
+            case "remove-birthday":
+                print(remove_birthday(args, book))
             case "birthdays":
                 print(birthdays(args, book))
             case "remove-phone":
